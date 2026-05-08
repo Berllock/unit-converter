a1394f3f18581b8b721c8070a7d55946aec16c09
Berllock <brenosouzaemp@gmail.com>
2026-05-08 16:58:12 -0300

    create weight enum convert

--- src/main/java/unit_converter/web/model/WeightUnit.java
+++ src/main/java/unit_converter/web/model/WeightUnit.java
@@ -2,7 +2,23 @@ package unit_converter.web.model;
 
 public enum WeightUnit {
 
-    miligram, gram, kilogram, ounce, pound
+    MILIGRAM(0.001),
+    GRAM(1.0),
+    KILOGRAM(1000.0),
+    OUNCE(28.3495),
+    POUND(453.592);
 
+    private final double gramsFactor;
 
+    WeightUnit(double gramsFactor){
+        this.gramsFactor = gramsFactor;
+    }
+
+    public double toBase(double value) {
+        return value * gramsFactor;
+    }
+
+    public double fromBase(double baseValue) {
+        return baseValue / gramsFactor;
+    }
 }
